3f690e951a91db0b79f5c3ec05aca30774a42e73
Artin <lengthmin@gmail.com>
2022-02-04 14:51:16 +0000

    chore: validate in action

--- scripts/validate-remote-secrets.ts
+++ scripts/validate-remote-secrets.ts
@@ -1,23 +1,22 @@
 import allSecretKeys from './secrets';
 import minimist from 'minimist';
-const argv = minimist(process.argv.slice(2));
-console.log(argv);
 import { execSync } from 'child_process';
 
+const argv = minimist(process.argv.slice(2));
+console.log(argv);
+
+const env = argv.env || process.env.BUILD_ENVIRONMENT;
+
 const result = execSync(
-  `./node_modules/.bin/wrangler secret list ${
-    argv.env ? `--env ${argv.env}` : ''
-  }`,
+  `./node_modules/.bin/wrangler secret list ${env ? `--env ${env}` : ''}`,
 );
 const remoteValue = JSON.parse(result.toString()) as {
   name: string;
   type: string;
 }[];
+
 const remoteKeys = remoteValue.map((v) => v.name);
-console.log(
-  `🚀 ~ file: validate-remote-secrets.ts ~ line 10 ~ result`,
-  remoteKeys,
-);
+console.log(`remoteKeys: `, remoteKeys);
 
 for (const key of allSecretKeys) {
   if (!remoteKeys.includes(key)) {
