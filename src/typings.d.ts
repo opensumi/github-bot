c8145966cd03aa061ca296a66d4e56a4770fa001
Artin <lengthmin@gmail.com>
2022-02-08 05:15:23 +0000

    fix: remove oauth app

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,5 +3,7 @@ declare const DINGTALK_WEBHOOK_URL: string;
 declare const DINGTALK_OUTGOING_TOKEN: string;
 declare const GH_WEBHOOK_SECRET: string;
 declare const GH_APP_ID: string;
+declare const GH_APP_CLIENT_ID: string;
+declare const GH_APP_CLIENT_SECRET: string;
 declare const GH_APP_WEBHOOK_SECRET: string;
 declare const GH_APP_PRIVATE_KEY: string;
