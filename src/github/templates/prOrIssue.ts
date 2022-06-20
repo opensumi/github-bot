89ab0cdb43d57d445d3d68934c6d818c3abd697c
Artin <lengthmin@gmail.com>
2022-06-20 13:49:56 +0000

    feat: do not display reopened issues

--- src/github/templates/prOrIssue.ts
+++ src/github/templates/prOrIssue.ts
@@ -24,7 +24,7 @@ function render(
   let action = payload.action as string;
 
   let shouldRenderBody = true;
-  if (['closed', 'edited'].includes(action)) {
+  if (['closed', 'edited', 'reopened'].includes(action)) {
     shouldRenderBody = false;
   }
 
