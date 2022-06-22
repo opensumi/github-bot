c19bebc022b791832489ee980796c14de1f0e088
Artin <lengthmin@gmail.com>
2022-06-22 12:42:41 +0000

    feat: update workflow content

--- src/github/templates/workflow.ts
+++ src/github/templates/workflow.ts
@@ -2,10 +2,13 @@ import { StringBuilder } from '@/utils';
 import { renderUserLink, titleTpl, textTpl, StopHandleError } from '.';
 import { Context } from '../app';
 import { ExtractPayload, MarkdownContent } from '../types';
+import { Octokit } from '@octokit/rest';
 
 export async function handleWorkflowRun(
   payload: ExtractPayload<'workflow_run'>,
-  ctx: Context,
+  ctx: Context & {
+    octokit?: Octokit;
+  },
 ): Promise<MarkdownContent> {
   const workflow = payload.workflow;
   const workflowRun = payload.workflow_run;
@@ -14,6 +17,18 @@ export async function handleWorkflowRun(
   if (workflowRun.path !== '.github/workflows/manual-release-rc.yml') {
     throw new StopHandleError('only rc release');
   }
+
+  if (!ctx.octokit) {
+    throw new StopHandleError('should have ctx octokit');
+  }
+
+  const checkRunsData = await ctx.octokit.checks.listForSuite({
+    owner: 'opensumi',
+    repo: 'core',
+    check_suite_id: workflowRun.check_suite_id,
+  });
+
+  const runs = checkRunsData.data.check_runs;
 
   const title = titleTpl(
     {
@@ -27,6 +42,10 @@ export async function handleWorkflowRun(
   const builder = new StringBuilder();
   builder.add(`Name: ${workflow.name}`);
   builder.add(`[Click me to see version detail](${workflowRun.html_url})`);
+
+  for (const run of runs) {
+    builder.add(`- Run: ${run.name}  \n\nSummary: ${run.output.summary}`);
+  }
 
   const text = textTpl(
     {
