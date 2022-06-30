80a565edffe2205968f67563a93ce957d7c60785
Artin <lengthmin@gmail.com>
2022-06-30 11:19:33 +0000

    fix: handle putData

--- src/ding/commands/common.ts
+++ src/ding/commands/common.ts
@@ -1,15 +1,21 @@
 import { cc, Context } from './base';
 import { code } from '../message';
 import { DingInfo, updateGroupInfo } from '../secrets';
+import { startsWith } from '@/command';
 
-cc.on('putData', async (bot, ctx: Context<Partial<DingInfo>>) => {
-  const info = {} as DingInfo;
-  if (ctx.parsed['defaultRepo']) {
-    info['defaultRepo'] = ctx.parsed['defaultRepo'];
-  }
-  await updateGroupInfo(bot.id, info);
-  await bot.replyText('更新信息成功');
-});
+cc.on(
+  'putData',
+  async (bot, ctx: Context<Partial<DingInfo>>) => {
+    const info = {} as DingInfo;
+    if (ctx.parsed['defaultRepo']) {
+      info['defaultRepo'] = ctx.parsed['defaultRepo'];
+    }
+    await updateGroupInfo(bot.id, info);
+    await bot.replyText('更新信息成功');
+  },
+  undefined,
+  startsWith,
+);
 
 cc.on('getGroupInfo', async (bot) => {
   await bot.reply(
