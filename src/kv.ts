bad4a5c285c43a9aa1affabe96f9a86ac4db37e2
Artin <lengthmin@gmail.com>
2022-07-01 03:04:04 +0000

    feat: add kv manager

---
+++ src/kv.ts
@@ -0,0 +1,21 @@
+export class KVManager<T> {
+  constructor(private prefix: string, private _id: string) {}
+
+  get fullId() {
+    return this.prefix + this._id;
+  }
+
+  async getJSON() {
+    return await WEBHOOKS_INFO.get<T>(this.fullId, 'json');
+  }
+
+  async setJSON(data: T) {
+    return await WEBHOOKS_INFO.put(this.fullId, JSON.stringify(data));
+  }
+
+  async updateJSON(data: Partial<T>) {
+    const oldData = (await this.getJSON()) ?? {};
+    const newData = Object.assign({}, oldData, data);
+    return await WEBHOOKS_INFO.put(this.fullId, JSON.stringify(newData));
+  }
+}
