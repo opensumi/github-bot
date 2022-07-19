dca6c93583aa9673d4311e8301be34777137511f
Artin <lengthmin@gmail.com>
2022-07-19 03:06:36 +0000

    test: fix testcase

--- test/handle.test.ts
+++ test/handle.test.ts
@@ -1,19 +1,27 @@
-import { handleRequest } from '@/handler';
-import { makeEdgeEnv } from 'edge-mock';
+import { makeEdgeEnv, EdgeKVNamespace } from 'edge-mock';
+makeEdgeEnv();
+
+import app from '@/index';
 
 declare const FetchEvent: any;
 declare const Request: any;
 
-describe('handle', () => {
+describe.skip('handle', () => {
   beforeEach(() => {
-    makeEdgeEnv();
     jest.resetModules();
   });
 
   test('handle Missing route', async () => {
     const request = new Request('/?foo=1', { method: 'POST', body: 'hello' });
     const event = new FetchEvent('fetch', { request });
-    const result = await handleRequest(event);
+    const result = await app.fetch(
+      request,
+      {
+        KV_PROD: new EdgeKVNamespace() as any,
+        HOST: 'https://localhost',
+      },
+      event,
+    );
     expect(result.status).toEqual(404);
   });
 });
