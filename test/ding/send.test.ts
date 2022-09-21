39f559aa4fbf1c1f08ea4b4510d9659caecb1610
Artin <lengthmin@gmail.com>
2022-09-21 07:19:19 +0000

    test: add testcase

---
+++ test/ding/send.test.ts
@@ -0,0 +1,67 @@
+let content: any | undefined;
+jest.mock('@/ding/utils', () => {
+  // Require the original module to not be mocked...
+  const originalModule = jest.requireActual('@/ding/utils');
+
+  return {
+    __esModule: true, // Use it when dealing with esModules
+    ...originalModule,
+    send: async (_content: any) => {
+      console.log('====================================');
+      console.log(_content);
+      console.log('====================================');
+      content = _content;
+    },
+  };
+});
+import { sendContentToDing } from '@/github/utils';
+
+describe('can send content to dingtalk', () => {
+  it('can filter event', async () => {
+    content = undefined;
+    await sendContentToDing(
+      {
+        a: 1,
+      },
+      'some.event',
+      {
+        githubSecret: '',
+        contentLimit: 300,
+        dingWebhooks: [
+          {
+            secret: 'test',
+            url: '',
+            event: [
+              'release.released',
+              'discussion_comment.created',
+              'discussion.created',
+            ],
+          },
+        ],
+      },
+    );
+    expect(content).toBe(undefined);
+  });
+  it('can filter event', async () => {
+    content = undefined;
+    const data = {
+      a: 1,
+    };
+    await sendContentToDing(data, 'discussion_comment.created', {
+      githubSecret: '',
+      contentLimit: 300,
+      dingWebhooks: [
+        {
+          secret: '',
+          url: '',
+          event: [
+            'release.released',
+            'discussion_comment.created',
+            'discussion.created',
+          ],
+        },
+      ],
+    });
+    expect(content).toBe(data);
+  });
+});
