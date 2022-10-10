d97217c9d398f728801a76669a8d96851d440ce7
Artin <lengthmin@gmail.com>
2022-10-10 05:58:16 +0000

    refactor: update ignition

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -2,10 +2,20 @@ import { DingController } from './ding/ding.controller';
 import { GitHubController } from './github/github.controller';
 import { WebhookController } from './webhook/webhook.controller';
 import { ProxyController } from './proxy/proxy.controller';
+import { BaseController } from './base/base.controller';
 
 export function ignition(hono: THono) {
-  new DingController(hono);
-  new GitHubController(hono);
-  new WebhookController(hono);
-  new ProxyController(hono);
+  const modules = [
+    DingController,
+    GitHubController,
+    WebhookController,
+    ProxyController,
+  ] as typeof BaseController[];
+
+  const instances = modules.map((M) => {
+    const instance = new M(hono);
+    return instance;
+  });
+
+  return instances;
 }
