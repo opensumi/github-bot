5cb3431a6077bee1b026b1931c90142eb182f657
Artin <lengthmin@gmail.com>
2022-10-11 14:29:00 +0000

    fix: handle not work

--- src/modules/base/base.controller.ts
+++ src/modules/base/base.controller.ts
@@ -1,11 +1,15 @@
 export class BaseController {
   post: THono['post'];
+  get: THono['get'];
   all: THono['all'];
   constructor(private hono: THono) {
-    this.handle();
+    this.get = hono.get.bind(hono);
     this.post = hono.post.bind(hono);
     this.all = hono.all.bind(hono);
+
+    this.handle();
   }
+
   handle() {
     throw new Error('Method Not Implemented');
   }
