d041af28ebaf0904f428233d0dd4204ae6a3f0f1
野声 <lijiacheng.ljc@antgroup.com>
2023-02-09 19:23:52 +0800

    chore: add transform github key

---
+++ scripts/transform-github-private-key.ts
@@ -0,0 +1,20 @@
+const argv = process.argv;
+const targetPath = argv[2];
+if (!targetPath) {
+  throw new Error('targetPath is required');
+}
+
+import { readFile, writeFile } from 'fs/promises';
+
+import execa from 'execa';
+
+async function main() {
+  const cmd = `openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in ${targetPath} -out ${targetPath}.pkcs8`;
+  await execa.command(cmd, { shell: true });
+  const text = await readFile(`${targetPath}.pkcs8`, 'utf-8');
+  const data = text.replaceAll('\n', '\\n');
+  await writeFile(`${targetPath}.pkcs8`, data, 'utf-8');
+  await execa.command(`cat ${targetPath}.pkcs8 | pbcopy`, { shell: true });
+}
+
+main();
