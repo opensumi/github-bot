b953cc597053e3b9d7ab8d158a12b9dfc4b9d4d2
野声 <lijiacheng.ljc@antgroup.com>
2023-03-02 10:41:43 +0800

    chore: remove unused code

--- src/lib/GPT-3-Encoder/build_encoder.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-
-const encoder = JSON.parse(fs.readFileSync(path.join(__dirname, './encoder.json')));
-
-console.log("Breaks stuff i think");
-
-fs.writeFileSync('./encoder2.js', `module.exports = ${JSON.stringify(encoder)};`);
--- src/lib/GPT-3-Encoder/index.d.ts
+++
@@ -1,7 +0,0 @@
-export function encode(text: string): number[];
-
-export function decode(tokens: number[]): string;
-
-export function countTokens(text: string): number;
-
-export function tokenStats(input: string | number[]): any;
