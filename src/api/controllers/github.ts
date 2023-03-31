676ebb065b02bd02cede9b8853d3d96f0024a406
lijiacheng.ljc <lijiacheng.ljc@antgroup.com>
2023-03-31 11:20:49 +0800

    feat: support revoke token

--- src/api/controllers/github.ts
+++ src/api/controllers/github.ts
@@ -1,3 +1,5 @@
+import { Octokit } from '@octokit/core';
+
 import { webhookHandler } from '@/github';
 import { initApp } from '@/github/app';
 import { GitHubKVManager } from '@/kv/github';
@@ -60,5 +62,59 @@ export function route(hono: THono) {
 
     const app = await initApp(setting);
     return c.json(await app.createInstallationAccessToken(installationId));
+  });
+
+  hono.delete('/github/installation-token/:id', async (c) => {
+    const id = c.req.param('id');
+    if (!id) {
+      return c.send.error(400, `Bad request(id: ${id})`);
+    }
+
+    const authorization = c.req.header('Authorization');
+    let flag: string | undefined;
+    if (authorization) {
+      if (authorization.startsWith('flag ')) {
+        flag = authorization.slice('flag '.length);
+      }
+    }
+
+    if (!flag) {
+      return c.send.error(401, 'Unauthorized: missing flag');
+    }
+
+    const token = c.req.header('Token');
+    if (!token) {
+      return c.send.error(400, 'Bad Request: missing token');
+    }
+
+    // 先查数据库有没有设置这个 id 对应的 installation id
+    const githubKVManager = new GitHubKVManager();
+    const setting = await githubKVManager.getAppSettingById(id);
+
+    if (!setting) {
+      return c.send.error(400, 'id not found in database');
+    }
+
+    if (!setting.installation || !setting.installation.flags) {
+      return c.send.error(400, 'no installation id found in database');
+    }
+
+    const { installation } = setting;
+    const installationId = installation.flags[flag];
+    if (typeof installationId === 'undefined') {
+      return c.send.error(400, 'flag not match');
+    }
+
+    const octokit = new Octokit({
+      auth: token,
+    });
+
+    const result = await octokit.request('DELETE /installation/token', {
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+    });
+
+    return c.json(result.data, result.status);
   });
 }
