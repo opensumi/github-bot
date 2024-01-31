5eb5dc5af0d2bcfdcb1ea95de3c88108a628da12
lijiacheng.ljc <lijiacheng.ljc@antgroup.com>
2024-01-31 15:46:11 +0800

    chore: remove unused connection

--- src/queue/worker/index.ts
+++ src/queue/worker/index.ts
@@ -1,8 +1,4 @@
-import { EventEmitter } from 'eventemitter3';
-
 export abstract class BaseWorker<T> {
-  protected emitter = new EventEmitter();
-
   queue: Message<T>[] = [];
 
   push(...message: Message<T>[]) {
