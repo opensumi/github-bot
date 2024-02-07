58d7f29f1999fd7b1702f6b9a75f8f7aed71febd
artin <artin@cat.ms>
2024-02-07 14:47:24 +0800

    chore: remove unused file

--- src/im/commands/constants.ts
+++ src/im/commands/constants.ts
@@ -6,16 +6,3 @@ export enum KnownRepo {
 export const ISSUE_REGEX = /^#(?<number>\d+)$/;
 export const REPO_REGEX =
   /^(?<owner>[a-zA-Z0-9][a-zA-Z0-9\-]*)\/(?<repo>[a-zA-Z0-9_\-.]+)$/;
-
-export enum TEAM_MEMBERS {
-  CONTRIBUTOR = 'contributor',
-  CORE_MEMBER = 'core-member',
-  MENTOR = 'mentor',
-  NONE = 'none',
-}
-
-export const TEAM_MEMBER_PR_REQUIREMENT = {
-  [TEAM_MEMBERS.CONTRIBUTOR]: 3,
-  [TEAM_MEMBERS.CORE_MEMBER]: 5,
-  [TEAM_MEMBERS.MENTOR]: 10,
-};
